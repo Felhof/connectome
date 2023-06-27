1f6aff1425e14cd3ecbc6fbc78a1b216be08a2a3
Felix Hofstätter <felixahofstaetter@gmail.com>
2023-06-27 11:36:52 +0100

    add sensitivity plotting scripts

---
+++ sensitivity_estimator.py
@@ -0,0 +1,113 @@
+# %%
+from functools import partial
+from typing import Callable, List
+
+import numpy as np
+import torch
+from transformer_lens import utils, HookedTransformer
+from transformers import AutoTokenizer
+from transformers.models.bert.modeling_bert import BertForMaskedLM
+
+# %%
+bert = BertForMaskedLM.from_pretrained("bert-large-cased")
+tokenizer = AutoTokenizer.from_pretrained("bert-large-cased")
+# %%
+def to_str_tokens(tokenizer, prompt) -> List[str]:
+    tokens = tokenizer(prompt, return_tensors="pt", padding=True)["input_ids"]
+    return tokenizer.batch_decode(tokens, clean_up_tokenization_spaces=False)
+
+# TODO: make sure corrpted sentences have same number of tokens when using gpt2 tokenizer
+def corrupt_sentences(model, tokenizer, sentences, n_masks=4, last_mask_idx=-1):
+
+    model.eval()
+    masked_sentences = []
+
+    token_ids = tokenizer(sentences, return_tensors="pt", padding=True)["input_ids"]
+
+    n_batch = len(token_ids)
+    s_len = len(token_ids[0])
+    if last_mask_idx == -1:
+        last_mask_idx = len(token_ids[0]) - 1
+    assert last_mask_idx > n_masks, "last_mask_idx must be larger than n_masks."
+
+    # mask tokens at n_mask random indices up to last_mask_idx
+    # don't mask idx 0 or s_len as they contain the CLS and SEP tokens.
+    # mask_indices = torch.randint(1, s_len, (n_batch, n_masks))
+    mask_indices = torch.sort(torch.randperm(last_mask_idx + 1)[:n_masks]).values.unsqueeze(0).expand(n_batch, -1) + 1
+    mask = torch.zeros(n_batch, s_len, dtype=torch.bool)
+    mask.scatter_(1, mask_indices, True)
+    token_ids.masked_fill_(mask, tokenizer.mask_token_id)
+
+    masked_sentences = tokenizer.batch_decode(token_ids[:,1:])
+    
+    # predict the masked tokens one by one
+    for n, index_column in enumerate(mask_indices.T):
+        out = model(token_ids).logits
+        preds = out[token_ids == tokenizer.mask_token_id]
+        top_preds = preds.max(-1).indices.reshape(n_batch, n_masks - n)
+        token_ids[range(n_batch), index_column] = top_preds[range(n_batch), 0]
+
+    corrupted_sentences = tokenizer.batch_decode(token_ids[:,1:])
+
+    return masked_sentences, corrupted_sentences
+# %%
+
+corrupt_sentences(
+    bert,
+    tokenizer,
+    [
+        # "The archaeologists discovered ancient ruins buried beneath the sand.",
+        # "He took a sip of the warm tea to soothe his sore throat.",
+        # "The hiker marveled at the breathtaking view from the mountaintop.",
+        "Alice was so tired when she got back home, so she went to bed. She had a very nice dream.",
+        "Lily likes cats and dogs. She asked her mum for a dog and her mum said no. So she asked her dad for a cat.",
+        "Alice and Jack walked up the street and met a girl in a red dress. The girl smiled and said her name was Jane."
+    ],
+    n_masks = 8,
+    last_mask_idx=16
+)
+
+# %%
+def patch_position(activation, hook, cache, source: int, target: int) -> None:
+    activation[:,target,:,:] = cache[:,source,:,:]
+
+# %%
+gpt2_small = HookedTransformer.from_pretrained("gpt2-small")
+# %%
+original_prompt = "When Alice and Bob walked to the store, Bob gave a book to"
+corrupted_prompt = "When Bob and Alice walked to the store, Alice gave a book to"
+# %%
+# Get cache from running clean and corrupted inputs
+original_logits, cache = gpt2_small.run_with_cache(original_prompt)
+_, corrupted_cache = gpt2_small.run_with_cache(corrupted_prompt)
+# %%
+original_logit_diff = (original_logits[0,-1,gpt2_small.to_single_token(" Alice")] - original_logits[0,-1,gpt2_small.to_single_token(" Bob")]).item()
+# %%
+# 
+# %%
+# Create cache for patching by taking only the source position of the key and value vectors
+# from the corrupted cache
+source_pos = 10
+target_pos = 14
+for layer in range(gpt2_small.cfg.n_layers):
+    cache[utils.get_act_name("v", layer)][:,source_pos] = corrupted_cache[utils.get_act_name("v", layer)][:,source_pos]
+    cache[utils.get_act_name("k", layer)][:,source_pos] = corrupted_cache[utils.get_act_name("k", layer)][:,source_pos]
+# %%
+hooks = [
+    (
+        utils.get_act_name("v", layer),
+        partial(patch_position, cache=cache[utils.get_act_name("v", layer)], source=source_pos, target=target_pos)
+    ) for layer in range(gpt2_small.cfg.n_layers)
+] + [
+    (
+        utils.get_act_name("k", layer),
+        partial(patch_position, cache=cache[utils.get_act_name("k", layer)], source=source_pos, target=target_pos)
+    ) for layer in range(gpt2_small.cfg.n_layers)
+]
+corrupted_logits = gpt2_small.run_with_hooks(
+    original_prompt,
+    fwd_hooks=hooks
+)
+# %%
+corrupted_logit_diff = (corrupted_logits[0,-1,gpt2_small.to_single_token(" Alice")] - corrupted_logits[0,-1,gpt2_small.to_single_token(" Bob")]).item()
+# %%
---
+++ sensitivity_plots.py
@@ -0,0 +1,134 @@
+# %%
+import gc
+
+from tqdm.autonotebook import tqdm
+import numpy as np
+import torch
+from transformer_lens import HookedTransformer
+
+import diego as d
+
+# %%
+torch.set_grad_enabled(False)
+# %%
+model = HookedTransformer.from_pretrained("gpt2-small")
+# %%
+original_prompt = "When John and Mary went to the store, John gave a book to"
+# %%
+def get_similar_word(model, word, distance=1):
+    def basically_equal(str1, str2):
+        str1 = str1.replace(" ", "").lower()
+        str2 = str2.replace(" ", "").lower()
+
+        if str1 == str2:
+            return True
+
+        if str1 + "s" == str2 or str2 + "s" == str1:
+            return True
+
+        return False
+
+    similarities = (model.W_E[model.to_single_token(word)] @ model.W_E.T).clone()
+    similar_token_ids = torch.argsort(similarities, descending=True).tolist()
+    found_similar_words = 0
+    for token_id in similar_token_ids:
+        similar_word = model.to_single_str_token(token_id)
+        if basically_equal(word, similar_word):
+            continue
+        found_similar_words += 1
+        if found_similar_words == distance:
+            return similar_word
+    return ""
+# %%
+get_similar_word(model, " Christmas", distance=7)
+torch.cuda.empty_cache()
+gc.collect()
+# %%
+def get_similar_word_softmax(model, word, temperature=1., k=20):
+    similarities = (model.W_E[model.to_single_token(word)] @ model.W_E.T).clone()
+    top_values, top_indices = torch.topk(similarities, k=k)
+    probabilites = (top_values / temperature).softmax(0)
+    replacement_id = top_indices[torch.distributions.Categorical(probabilites).sample()]
+    return model.to_single_str_token(replacement_id.item())
+
+# %%
+def corrupt_prompt_with_similar_words(model, original_str_tokens, replace_pos_up_to, n_replacements, distance):
+    str_tokens = original_str_tokens.copy()
+    ids_to_replace = np.random.choice(range(1, replace_pos_up_to), n_replacements, replace=False)
+    for id in ids_to_replace:
+        str_tokens[id] = get_similar_word(model, str_tokens[id], distance=distance)
+        torch.cuda.empty_cache()
+        gc.collect()
+    corrupted_prompt = model.tokenizer.batch_decode([[model.to_single_token(t) for t in str_tokens][1:]])
+    return corrupted_prompt
+# %%
+def corrupt_prompt_with_softmax(model, original_str_tokens, replace_pos_up_to, temperature):
+    str_tokens = original_str_tokens.copy()
+
+# %%
+distances = [4, 8, 12, 16]
+n_samples = 20
+n_replacements = 4
+source_pos = [9, 10, 11]
+replace_pos_up_to = min(source_pos)
+target_pos = [14] * len(source_pos)
+original_str_tokens = model.to_str_tokens(original_prompt)
+metric = d.logit_diff_metric(model, ' John', ' Mary')
+original_predictions = model(original_prompt)[0]
+
+results = {pos: [] for pos in source_pos}
+for distance in distances:
+    corrupted_prompts = []
+    distance_results = []
+    for _ in range(n_samples):
+        corrupted_prompt = corrupt_prompt_with_similar_words(model, original_str_tokens, replace_pos_up_to, n_replacements, distance)
+        corrupted_prompts.append("".join(corrupted_prompt))
+
+    for corrupt_prompt in tqdm(corrupted_prompts):
+        intervention = d.CorruptIntervention(model, original_prompt, corrupt_prompt) # type: ignore
+
+        with intervention.batch_hooks(model, sources=source_pos, targets=target_pos):
+            logits = model([original_prompt] * len(source_pos))
+
+        for logit, source, target in zip(logits, source_pos, target_pos):
+            strength = metric(original_predictions, logit)
+            results[source].append(strength.item())
+
+
+# %%
+import plotly.express as px
+
+x_axis = []
+y_axis = []
+colors = []
+str_tokens = model.to_str_tokens(original_prompt)
+color_map = {pos: str_tokens[pos] for pos in source_pos}
+
+for pos in source_pos:
+    y_axis.extend([abs(r) for r in results[pos]])
+    for distance in distances:
+        x_axis.extend([distance] * n_samples)
+    colors.extend([color_map[pos]] * n_samples * len(distances))
+
+fig = px.scatter(
+    x=x_axis,
+    y=y_axis,
+    color=colors
+)
+ 
+fig.show()
+
+# %%
+p = 90
+
+y_axis = []
+for pos in source_pos:
+    values = []
+    for idx, distance in enumerate(distances):
+        pos_results_for_distance = [abs(r) for r in results[pos][idx*n_samples:(idx+1)*n_samples]]
+        values.append(np.percentile(pos_results_for_distance, p))
+    y_axis.append(values)
+# %%
+px.line(x=distances, y=y_axis, labels=[str_tokens[pos] for pos in source_pos])
+
+# %%
